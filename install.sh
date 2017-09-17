c6d7c14216670e7c84e8419feed548f6b5426390
David Nguyen <hungphuongjc@gmail.com>
2017-09-17 13:41:59 +0800

    restore iterm settings using stow

--- install.sh
+++ install.sh
@@ -31,11 +31,6 @@ echo "================================  Stow ===================================
 # create symlink at home directory for these packages
 ruby ./backup_and_stow.rb
 
-# Specify the preferences directory
-defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/dotfiles/iterm"
-# Tell iTerm2 to use the custom preferences in the directory
-defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
-
 echo "=============================  VimPlug Update ============================"
 
 # install VimPlug plugins

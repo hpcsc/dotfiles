25a089fa89b3471d1a736004fa64330eaa0fc6e1
David Nguyen <hpcsc@users.noreply.github.com>
2017-10-19 23:19:47 +0800

    use sudo for individual commands

--- install-debian.sh
+++ install-debian.sh
@@ -2,21 +2,16 @@
 
 echo "Setting up your Linux..."
 
-sudo -v
-
-# Keep-alive: update existing `sudo` time stamp until `install.sh` has finished
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
 # in case add-apt-repository is missing
-apt-get update
-apt-get install -y software-properties-common python-software-properties
+sudo apt-get update
+sudo apt-get install -y software-properties-common python-software-properties
 
 # install additional tools
-add-apt-repository ppa:aacebedo/fasd
-apt-get update
+sudo add-apt-repository ppa:aacebedo/fasd
+sudo apt-get update
 
 # Install essential tools
-apt-get install -y zsh fasd stow vim
+sudo apt-get install -y zsh fasd stow vim
 
 echo "=========================== Prezto Setup ================================="
 

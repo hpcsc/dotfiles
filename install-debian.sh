8309a2fdadbd639c2771aff5cf257738f809b3d5
David Nguyen <hpcsc@users.noreply.github.com>
2017-12-07 09:44:00 +0800

    prompt to install asdf plugin

--- install-debian.sh
+++ install-debian.sh
@@ -54,15 +54,7 @@ git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.4.0
 source ~/.asdf/asdf.sh
 source ~/.asdf/completions/asdf.bash
 
-# nodejs plugin for asdf
-asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring # Imports Node.js release team's OpenPGP keys to main keyring
-
-# ruby plugin for asdf
-asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
-
-# golang plugin for asdf
-asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
+./install-asdf-plugins.sh
 
 echo "=============================  Antigen ==================================="
 sudo sh -c 'mkdir -p /usr/share/antigen && curl -L git.io/antigen > /usr/share/antigen/antigen.zsh'

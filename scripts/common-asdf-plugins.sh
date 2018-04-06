21427c08731a547e7ddd688dd07960e2db17bed4
David Nguyen <hpcsc@users.noreply.github.com>
2018-04-06 20:31:58 +0800

    fix checking for asdf installed

--- scripts/common-asdf-plugins.sh
+++ scripts/common-asdf-plugins.sh
@@ -14,7 +14,7 @@ function install() {
     fi
 }
 
-(command -v asdf >/dev/null 2>&1 && echo_green "=== ASDF is already installed, skipped") || {
+([ -d ~/.asdf ] && echo_green "=== ASDF is already installed, skipped") || {
   asdf_version=$(curl https://api.github.com/repos/asdf-vm/asdf/tags | jq -r '.[0].name')
   echo_yellow "=== Checking out asdf at tag $asdf_version"
   git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch $asdf_version || {
@@ -59,6 +59,7 @@ exit_code=0
 # python plugin for asdf
 ((asdf plugin-list | grep python >/dev/null 2>&1) && echo_green "=== ASDF Python plugin is already installed, skipped") || {
   asdf plugin-add python https://github.com/tuvistavie/asdf-python.git
+  asdf list-all python
   install 'python' || {
     echo_red "=== Failed to install asdf python"
     exit_code=1

905b4f41509085bb1f5a11eff44d0d14819909f1
David Nguyen <hpcsc@users.noreply.github.com>
2018-04-07 10:55:06 +0800

    separate macos stow from common stow

--- install.sh
+++ install.sh
@@ -13,15 +13,29 @@ export -f echo_blue
 export -f echo_purple
 export -f echo_cyan
 
-rm -f ./install.log
+function execute_scripts() {
+  local scripts_array_name=$1[@]
+  local scripts=("${!scripts_array_name}")
+  for i in "${scripts[@]}"; do
+    echo_purple "=============================  $i ======================================="
+
+    $i && echo "OK $i" >> ./install.log || {
+      echo_red "FAILED $i"
+      echo "FAILED $i" >> ./install.log
+    }
+  done
+}
 
 echo_purple "SETTING UP YOUR MACHINE..."
+rm -f ./install.log
 
-scripts=(
+macos_scripts=(
   ./scripts/macos-keep-sudo.sh
   ./scripts/macos-install-homebrew.sh
   ./scripts/macos-brew-bundle.sh
+)
 
+ubuntu_scripts=(
   ./scripts/ubuntu-install-sudo.sh
   ./scripts/ubuntu-install-required-packages.sh
   ./scripts/ubuntu-fasd.sh
@@ -30,7 +44,9 @@ scripts=(
   ./scripts/ubuntu-docker.sh
   ./scripts/ubuntu-antigen.sh
   ./scripts/ubuntu-ripgrep.sh
+)
 
+common_scripts=(
   ./scripts/common-asdf-plugins.sh
   ./scripts/common-stow.sh
   ./scripts/common-vim.sh
@@ -39,14 +55,14 @@ scripts=(
   ./scripts/common-working-folders.sh
 )
 
-for i in "${scripts[@]}"; do
-  echo_purple "=============================  $i ======================================="
+macos_after_scripts=(
+  ./scripts/macos-stow.sh
+)
 
-  $i && echo "OK $i" >> ./install.log || {
-    echo_red "FAILED $i"
-    echo "FAILED $i" >> ./install.log
-  }
-done
+is_macos && (execute_scripts macos_scripts)
+is_ubuntu && (execute_scripts ubuntu_scripts)
+execute_scripts common_scripts
+is_macos && (execute_scripts macos_after_scripts)
 
 # This should be run last because this will reload the shell
-source ./scripts/macos-settings.sh
+is_macos && source ./scripts/macos-settings.sh
--- scripts/macos-stow.sh
+++ scripts/macos-stow.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+is_macos || exit 0
+
 [ -f ~/.asdf/asdf.sh ] >/dev/null 2>&1 || {
   echo_red "=== ASDF (ruby plugin) must be installed before executing this script"
   exit 1
@@ -9,12 +11,8 @@ set -e
 
 source ~/.asdf/asdf.sh
 
-common_packages=(
-  git
-  vim
-  zsh
-  tmux
-  tig
+macos_packages=(
+  Applications
 )
 
 function backup() {
@@ -23,24 +21,18 @@ function backup() {
     local backup_folder_name=~/dotfiles_backup
     mkdir -p $backup_folder_name
 
-    for i in "${common_packages[@]}"; do
+    for i in "${macos_packages[@]}"; do
       echo_yellow "=== Backing up folder $i"
-      ruby ./scripts/common-backup.rb "./link/common/$i" "$backup_folder_name"
+      ruby ./scripts/common-backup.rb "./link/macos/$i" "$backup_folder_name"
     done
   fi
 }
 
 function stow_packages() {
-  for i in "${common_packages[@]}"; do
-    stow -vv --dir=./link/common --target="$HOME" --stow $i || echo_red "Failed to stow $i"
+  for i in "${macos_packages[@]}"; do
+    stow -vv --dir=./link/macos --target="$HOME" --stow $i || echo_red "Failed to stow $i"
   done
-
-  echo_yellow "=== Stowing .config folder"
-  mkdir -p ~/.config
-  stow -vv --dir=./link/common/dot-config --target="$HOME/.config" --stow .config || echo_red "Failed to stow .config folder"
 }
 
 backup
 stow_packages
-
-exit 0

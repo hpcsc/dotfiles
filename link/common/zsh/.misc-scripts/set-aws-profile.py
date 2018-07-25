7ef079245f104745d7fc8766fc6033bdd6cfbb93
David Nguyen <hpcsc@users.noreply.github.com>
2018-07-25 22:50:46 +0800

    refactor set-aws-profile.py script

--- link/common/zsh/.misc-scripts/set-aws-profile.py
+++ link/common/zsh/.misc-scripts/set-aws-profile.py
@@ -4,57 +4,64 @@ import os
 import configparser
 import sys
 
+def ensure_file_exists(file_path):
+    if not os.path.exists(file_path):
+        print(f'=== {file_path} not found')
+        sys.exit(1)
+
+def read_config_file(file_path):
+    ensure_file_exists(file_path)
+    config_file = configparser.ConfigParser()
+    config_file.read(file_path)
+    return config_file
+
+def create_default_if_not_exists(config_file, config_file_name):
+    if not 'default' in config_file:
+        print(f'=== profile [default] not found in {config_file_name} file, creating one')
+        config_file['default'] = {}
+
+def write_config_to_file(config_file_path, config_file):
+    with open(config_file_path, 'w') as opened_file:
+        config_file.write(opened_file)
+
+def is_assumed_role_profile(profile, config):
+    if not profile in config:
+        return False
+
+    return config[profile]['role_arn'] and config[profile]['source_profile']
+
+def set_profile(profile):
+    credentials_file_path = f'{os.path.expanduser("~")}/.aws/credentials'
+    credentials_file = read_config_file(credentials_file_path)
+
+    config_file_path = f'{os.path.expanduser("~")}/.aws/config'
+    config_file = read_config_file(config_file_path)
+
+    # sys.exit(1)
+
+    create_default_if_not_exists(credentials_file, 'credentials')
+    create_default_if_not_exists(config_file, 'config')
+
+    if profile in credentials_file:
+        print(f'=== setting AWS profile [{profile}] as default profile')
+        credentials_file['default']['aws_access_key_id'] = credentials_file[profile]['aws_access_key_id']
+        credentials_file['default']['aws_secret_access_key'] = credentials_file[profile]['aws_secret_access_key']
+        config_file['default'].pop('role_arn', None)
+        config_file['default'].pop('source_profile', None)
+        write_config_to_file(credentials_file_path, credentials_file)
+        write_config_to_file(config_file_path, config_file)
+    elif is_assumed_role_profile(profile, config_file):
+        print(f'=== assuming AWS profile [{profile}]')
+        config_file['default']['role_arn'] = config_file[profile]['role_arn']
+        config_file['default']['source_profile'] = config_file[profile]['source_profile']
+        write_config_to_file(config_file_path, config_file)
+    else:
+        print(f'=== profile [{profile}] not found in either credentials or config file')
+        sys.exit(1)
+
 if len(sys.argv) == 1:
     print('=== this script requires one argument, usage: set-aws-profile.py target-profile')
     sys.exit(1)
 
 profile = sys.argv[1]
-
-credentials_file_path = f'{os.path.expanduser("~")}/.aws/credentials'
-if not os.path.exists(credentials_file_path):
-    print(f'=== AWS credentials file at {credentials_file_path} not found')
-    sys.exit(1)
-
-config_file_path = f'{os.path.expanduser("~")}/.aws/config'
-if not os.path.exists(config_file_path):
-    print(f'=== AWS config file at {config_file_path} not found')
-    sys.exit(1)
-
-credentials_config = configparser.ConfigParser()
-credentials_config.read(credentials_file_path)
-
-config = configparser.ConfigParser()
-config.read(config_file_path)
-
-profile_is_in_credentials_file = profile in credentials_config
-profile_is_in_config_file = profile in config
-
-if (not profile_is_in_credentials_file) and (not profile_is_in_config_file):
-    print(f'=== profile [{profile}] not found in either credentials or config file')
-    sys.exit(1)
-
-if not 'default' in credentials_config:
-    print('=== profile [default] not found in credentials file, creating one')
-    credentials_config['default'] = {}
-
-if not 'default' in config:
-    print('=== profile [default] not found in config file, creating one')
-    config['default'] = {}
-
-if profile_is_in_credentials_file:
-    print(f'=== setting AWS profile [{profile}] as default profile')
-    credentials_config['default']['aws_access_key_id'] = credentials_config[profile]['aws_access_key_id']
-    credentials_config['default']['aws_secret_access_key'] = credentials_config[profile]['aws_secret_access_key']
-    config['default'].pop('role_arn', None)
-    config['default'].pop('source_profile', None)
-
-if profile_is_in_config_file:
-    print(f'=== assuming AWS profile [{profile}]')
-    config['default']['role_arn'] = config[profile]['role_arn']
-    config['default']['source_profile'] = config[profile]['source_profile']
-
-with open(credentials_file_path, 'w') as credentials_file:
-    credentials_config.write(credentials_file)
-
-with open(config_file_path, 'w') as config_file:
-    config.write(config_file)
+set_profile(profile)

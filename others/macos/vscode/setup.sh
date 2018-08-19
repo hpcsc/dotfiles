0f4839eaf772d7c41e4876ed88d1e22a1a69fffa
David Nguyen <hpcsc@users.noreply.github.com>
2018-08-19 16:21:13 +0800

    reorganize vscode stow folder

--- others/macos/vscode/setup.sh
+++ others/macos/vscode/setup.sh
@@ -4,34 +4,8 @@ set -e
 
 source ~/.functions/misc
 
-[ -f ~/.asdf/asdf.sh ] >/dev/null 2>&1 || {
-  echo_red "=== ASDF (ruby plugin) must be installed before executing this script"
-  exit 1
-}
-
-source ~/.asdf/asdf.sh
-
-macos_packages=(
-  Applications
-)
-
-function backup() {
-  read -p "=== Backing up before stowing? (Yn)" confirm_backup
-  if [ "$confirm_backup" = "" ] || [ "$confirm_backup" = "y" ] || [ "$confirm_backup" = "Y" ]; then
-    local backup_folder_name=~/dotfiles_backup
-    mkdir -p $backup_folder_name
-
-    for i in "${macos_packages[@]}"; do
-      echo_yellow "=== Backing up folder $i"
-      ruby ./scripts/common-backup.rb "./link/macos/$i" "$backup_folder_name"
-    done
-  fi
-}
-
 function stow_packages() {
-  for i in "${macos_packages[@]}"; do
-    stow -vv --dir=./link/macos --target="$HOME" --stow $i || echo_red "Failed to stow $i"
-  done
+  stow -vv --dir=./link/macos/Applications --target="$HOME" --stow vscode || echo_red "Failed to stow vscode settings"
 }
 
 function install_extensions() {
@@ -49,6 +23,5 @@ function install_extensions() {
 }
 
 mkdir -p /Library
-backup
 stow_packages
 install_extensions

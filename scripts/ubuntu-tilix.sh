290d32d370359453bdeb8c81b3ff4a3f5ed2d16c
David Nguyen <hpcsc@users.noreply.github.com>
2019-06-20 00:13:07 +1000

    add sudo when installing tilix in ubuntu

--- scripts/ubuntu-tilix.sh
+++ scripts/ubuntu-tilix.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-sudo apt-get update && apt-get install -y tilix && dconf load /com/gexperts/Tilix/ < ~/dotfiles/others/ubuntu/tilix.dconf
+sudo apt-get update && sudo apt-get install -y tilix && dconf load /com/gexperts/Tilix/ < ~/dotfiles/others/ubuntu/tilix.dconf

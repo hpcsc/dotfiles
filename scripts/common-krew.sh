f3038642f5f6879472c72b91a235840bd30ccc19
David Nguyen <hpcsc@users.noreply.github.com>
2019-12-27 22:51:33 +1100

    update krew installation script

--- scripts/common-krew.sh
+++ scripts/common-krew.sh
@@ -2,17 +2,22 @@
 
 set -e
 
-( [ -d "$HOME/.krew" ] && echo_green "=== krew is already installed, skipped") || {
+[ -d "$HOME/.krew" ] && {
+  echo_green "=== krew is already installed, updating"
+  ${HOME}/.krew/bin/kubectl-krew update
+} || {
   set -x
 
   echo_yellow "=== Installing krew to manage kubectl plugins"
 
   cd "$(mktemp -d)"
 
-  latest_release_version=$(curl https://api.github.com/repos/GoogleContainerTools/krew/releases/latest | jq -r '.tag_name')
-  curl -fsSLO "https://storage.googleapis.com/krew/${latest_release_version}/krew.{tar.gz,yaml}"
+  latest_release_version=$(curl https://api.github.com/repos/kubernetes-sigs/krew/releases/latest | jq -r '.tag_name')
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/download/${latest_release_version}/krew.{tar.gz,yaml}"
 
   tar zxvf krew.tar.gz
 
-  ./krew-"$(uname | tr '[:upper:]' '[:lower:]')_amd64" install --manifest=krew.yaml --archive=krew.tar.gz
+  KREW=./krew-"$(uname | tr '[:upper:]' '[:lower:]')_amd64"
+  ${KREW} install --manifest=krew.yaml --archive=krew.tar.gz
+  ${KREW} update
 }

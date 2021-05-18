b861e288ed52edfaeae38e32178f6bc5abc6f0d5
David Nguyen <hpcsc@users.noreply.github.com>
2021-05-18 20:00:08 +1000

    Separate ubuntu and macos vscode configs

--- others/ubuntu/vscode/setup.sh
+++ others/ubuntu/vscode/setup.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-set -e
+set -euo pipefail
 
 source ./load-zsh-autoload-as-functions.sh
 
 function stow_packages() {
-  stow -vv --dir='./link/macos/Applications/vscode/Library/Application Support' --target="$HOME/.config/Code" --stow Code || echo_red "Failed to stow vscode settings"
+  stow -vv \
+    --dir=./link/ubuntu/vscode/.config \
+    --target="$HOME/.config/Code" \
+    --stow Code || echo_red "Failed to stow vscode settings"
 }
 
-function install_extensions() {
+function sync_extensions() {
   command -v code >/dev/null 2>&1 || {
       echo_yellow "VSCode executable is not in Path, exiting"
       exit 1
@@ -20,8 +23,20 @@ function install_extensions() {
       echo_yellow "=== Installing $extension"
       code --install-extension $extension
   done <./others/ubuntu/vscode/extensions
+
+  # get list of extensions that are installed but not in the extensions files
+  # need to suppress error code from diff since it returns code 1 when there's difference
+  TO_UNINSTALL=( $(diff <(code --list-extensions) \
+                      <(cat ./others/ubuntu/vscode/extensions) \
+                      --old-line-format="%L" \
+                      --new-line-format="" \
+                      --unchanged-line-format="" || true) )
+  for extension in "${TO_UNINSTALL[@]}"; do
+      echo_yellow "=== Uninstalling $extension"
+      # code --uninstall-extension $extension
+  done
 }
 
 mkdir -p "$HOME/.config/Code"
 stow_packages
-install_extensions
+sync_extensions

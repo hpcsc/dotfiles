f489f87b9c7f42976817eb28ff8340f2a48ebd5a
David Nguyen <hpcsc@users.noreply.github.com>
2021-05-24 11:37:09 +1000

    Install copyq as clipboard manager in ubuntu

--- scripts/ubuntu-install-common-tools.sh
+++ scripts/ubuntu-install-common-tools.sh
@@ -5,8 +5,17 @@ set -e
 is_ubuntu || exit 0
 
 if [[ -n "${DISPLAY}" ]]; then
-  echo_yellow "=== GUI is detected, installing gnome-tweak-tool"
+  echo_yellow "=== GUI is detected"
+
+  echo_yellow "installing gnome-tweak-tool"
   sudo apt-get install -y gnome-tweak-tool
+
+  echo_yellow "=== Adding copyq apt repository"
+  sudo add-apt-repository ppa:hluk/copyq
+  sudo apt update
+
+  echo_yellow "=== Installing copyq"
+  sudo apt install -y copyq
 fi
 
 # Install essential tools
---
+++ scripts/ubuntu-stow.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+is_ubuntu || exit 0
+
+declare -A packages=( 
+  [copyq]="$HOME/.config/copyq"
+  [vscode]="$HOME/.config/Code/User"
+)
+
+function stow_packages() {
+  for package in "${!packages[@]}"; do
+    stow -vv \
+      --dir=./link/ubuntu \
+      --target="${packages[$package]}" \
+      --stow $package || echo_red "Failed to stow $package"
+  done
+}
+
+stow_packages
+
+exit 0

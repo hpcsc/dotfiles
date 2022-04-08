9cef6c2e5bf6978622a612ec5442c2d5f585a27e
David Nguyen <hpcsc@users.noreply.github.com>
2022-04-09 00:08:24 +1000

    Install fira font from nerd fonts
    Nerd fonts adds many glyphs on top of FiraCode font

--- scripts/macos-fonts.sh
+++ scripts/macos-fonts.sh
@@ -4,14 +4,17 @@ set -e
 
 is_macos || exit 0
 
-latest_fonts_from_github=$(curl https://api.github.com/repos/tonsky/FiraCode/releases/latest | \
+rm -rf /tmp/Fira.zip /tmp/Fira
+
+latest_fonts_from_github=$(curl https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest | \
   jq -r '.assets[] | select(.name | test("FiraCode.*?zip")) | .browser_download_url')
 
 echo "=== Downloading Fira Code from ${latest_fonts_from_github}"
 curl -L ${latest_fonts_from_github} -o /tmp/Fira.zip
 
-rm -rf /tmp/Fira
 unzip /tmp/Fira.zip -d /tmp/Fira
 
 echo "=== Installing Fira Code fonts to $HOME/Library/Fonts"
-cp -vf /tmp/Fira/ttf/* $HOME/Library/Fonts
+cp -vf /tmp/Fira/* $HOME/Library/Fonts
+
+rm -rf /tmp/Fira.zip /tmp/Fira

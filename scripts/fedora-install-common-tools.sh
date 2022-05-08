8b358f59743e901590297e886c1a9f7919546ade
David Nguyen <hpcsc@users.noreply.github.com>
2022-05-08 12:25:18 +1000

    Install direnv using asdf

--- scripts/fedora-install-common-tools.sh
+++ scripts/fedora-install-common-tools.sh
@@ -5,7 +5,7 @@ set -e
 is_fedora || exit 0
 
 # Install essential tools
-for i in curl zsh stow vim tree jq tmux tig direnv rsync neovim python3-neovim ripgrep; do
+for i in curl zsh stow vim tree jq tmux rsync neovim python3-neovim ripgrep; do
   echo_yellow "=== Installing $i"
   sudo dnf install -y $i
   echo_green "Installed $i"

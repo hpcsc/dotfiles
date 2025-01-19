6fd5cf2ab25c0486d84aa823fc67489654b6ea58
David Nguyen <hpcsc@users.noreply.github.com>
2025-01-19 20:03:36 +1100

    Install yazi full border plugin

--- scripts/common-configure-yazi.sh
+++ scripts/common-configure-yazi.sh
@@ -4,9 +4,16 @@ set -e
 
 # must be after mise
 
-if [ -z "$(~/.local/bin/mise exec cargo:yazi-cli -- ya pack -l | grep gruvbox-dark)" ]; then
-  echo_yellow "=== Installing yazi gruvbox-dark flavour"
-  ~/.local/bin/mise exec cargo:yazi-cli -- ya pack -a bennyyip/gruvbox-dark
-else
-  echo_yellow "=== yazi gruvbox-dark flavour exists, skip installation"
-fi
+install() {
+  local name=$1
+
+  if [ -z "$(~/.local/bin/mise exec cargo:yazi-cli -- ya pack -l | grep ${name})" ]; then
+    echo_yellow "=== Installing yazi ${name}"
+    ~/.local/bin/mise exec cargo:yazi-cli -- ya pack -a ${name}
+  else
+    echo_yellow "=== yazi ${name} exists, skip installation"
+  fi
+}
+
+install bennyyip/gruvbox-dark
+install yazi-rs/plugins:full-border

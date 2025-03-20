c6848b6a8ee0b48e312a6e6e62b32708ff2775e8
David Nguyen <hpcsc@users.noreply.github.com>
2025-03-20 22:19:20 +1100

    Install tmux plugins using tpm

--- scripts/common-tmux.sh
+++ scripts/common-tmux.sh
@@ -7,8 +7,12 @@ if [ -d ~/.tmux/plugins/tpm ]; then
   pushd ~/.tmux/plugins/tpm
   git pull -r
   popd
+  echo_yellow "=== updating tmux plugins using tpm"
+  ~/.tmux/plugins/tpm/bin/update_plugins all
 else
   echo_yellow "=== installing tmux plugin manager"
   mkdir -p ~/.tmux/plugins
   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+  echo_yellow "=== installing tmux plugins using tpm"
+  ~/.tmux/plugins/tpm/bin/install_plugins
 fi

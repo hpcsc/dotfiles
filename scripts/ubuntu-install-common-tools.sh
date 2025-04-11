17a6b91d307eef034e2c756ce2c3326064e95d4c
David Nguyen <hpcsc@users.noreply.github.com>
2025-04-11 18:55:08 +1000

    Install neovim using mise

--- scripts/ubuntu-install-common-tools.sh
+++ scripts/ubuntu-install-common-tools.sh
@@ -25,17 +25,3 @@ for i in zsh stow vim tree jq tmux rsync; do
   command -v $i >/dev/null 2>&1 || echo_red "Failed to install $i"
 done
 
-(command -v nvim >/dev/null && echo_green "=== Neovim is already installed, skipped")|| {
-  if [[ "$(grep DISTRIB_RELEASE /etc/lsb-release | cut -d'=' -f2)" < "18.04" ]]; then
-    echo_yellow "=== Adding neovim apt repository"
-    sudo add-apt-repository -r -y ppa:neovim-ppa/stable
-    sudo add-apt-repository -y ppa:neovim-ppa/stable
-    sudo apt-get update
-
-    echo_yellow "=== Installing Neovim"
-    sudo apt-get install -y neovim
-  else
-    echo_yellow "=== Installing Neovim"
-    sudo apt install -y neovim
-  fi;
-}

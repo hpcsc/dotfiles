45680df1ac9ae98b54cf2bdfee3b1b4dd179463b
David Nguyen <hpcsc@users.noreply.github.com>
2025-06-23 23:33:55 +1000

    Make rust tools available to mise install

--- scripts/common-mise-global.sh
+++ scripts/common-mise-global.sh
@@ -4,4 +4,5 @@ set -e
 
 # need to be after stow script since this needs ~/.config/mise/config.toml
 echo_yellow "=== Installing global tools with mise"
+export PATH="$HOME/.cargo/bin:$PATH"
 MISE_EXPERIMENTAL=true ~/.local/bin/mise --cd ~/ --yes install

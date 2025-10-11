3784578c173fc2ce57c494d026e2ea05f2de8dcc
David Nguyen <hpcsc@users.noreply.github.com>
2025-10-11 15:47:16 +1100

    Add some additional customization of vim airline

--- link/common/dot-config/.config/nvim/lua/plugins/airline.lua
+++ link/common/dot-config/.config/nvim/lua/plugins/airline.lua
@@ -3,6 +3,11 @@ return {
 
 	init = function()
 		vim.g["airline#extensions#tabline#enabled"] = 1
+		vim.g["airline#extensions#tabline#formatter"] = "unique_tail"
+    vim.g["airline#extensions#nerdtree_statusline"] = 0
+    vim.g["airline_skip_empty_sections"] = 1
+    vim.g["airline_section_b"] = ""
+		vim.g["airline_powerline_fonts"] = 1
 		-- workaround for wide character issue in NeoVim 0.10.*
 		vim.g["airline#extensions#whitespace#symbol"] = "!"
 	end,

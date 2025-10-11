557f2d8b821c4e876e34b73f684c096f416fbbb0
David Nguyen <hpcsc@users.noreply.github.com>
2025-10-11 15:49:10 +1100

    Reformat wezterm lua code

--- link/common/dot-config/.config/wezterm/custom/init.lua
+++ link/common/dot-config/.config/wezterm/custom/init.lua
@@ -1,11 +1,11 @@
-local modules = {'keys', 'ui'}
+local modules = { "keys", "ui" }
 
 function apply_to_config(config)
-    for i, module in ipairs(modules) do
-        require('custom.' .. module).apply_to_config(config)
-    end
+	for i, module in ipairs(modules) do
+		require("custom." .. module).apply_to_config(config)
+	end
 end
 
 return {
-    apply_to_config = apply_to_config
+	apply_to_config = apply_to_config,
 }

a9014b6a07a9bfe38d792e20469db5ca9bdfcaf8
David Nguyen <hpcsc@users.noreply.github.com>
2025-10-11 15:49:10 +1100

    Reformat wezterm lua code

--- link/common/dot-config/.config/wezterm/custom/ui.lua
+++ link/common/dot-config/.config/wezterm/custom/ui.lua
@@ -1,41 +1,41 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 local module = {}
 
 function module.apply_to_config(config)
-    config.color_scheme = 'GruvboxDark'
-    config.initial_rows = 300
-    config.initial_cols = 600
-    config.scrollback_lines = 10000
-    config.hide_tab_bar_if_only_one_tab = true
-    config.font = wezterm.font {
-        family = 'Fira Mono for Powerline',
-    }
+	config.color_scheme = "GruvboxDark"
+	config.initial_rows = 300
+	config.initial_cols = 600
+	config.scrollback_lines = 10000
+	config.hide_tab_bar_if_only_one_tab = true
+	config.font = wezterm.font({
+		family = "Fira Mono for Powerline",
+	})
 
-    config.font_size = 16
-    config.window_frame = {
-        font_size = 16.0
-    }
+	config.font_size = 16
+	config.window_frame = {
+		font_size = 16.0,
+	}
 
-    config.colors = {
-        tab_bar = {
-            active_tab = {
-                bg_color = '#7c6f64',
-                fg_color = '#3c3836'
-            },
-            inactive_tab = {
-                bg_color = '#504945',
-                fg_color = '#808080'
-            }
-        }
-    }
+	config.colors = {
+		tab_bar = {
+			active_tab = {
+				bg_color = "#7c6f64",
+				fg_color = "#3c3836",
+			},
+			inactive_tab = {
+				bg_color = "#504945",
+				fg_color = "#808080",
+			},
+		},
+	}
 
-    config.window_padding = {
-        top = 0,
-        left = 0,
-        right = 0,
-        bottom = 0,
-    }
+	config.window_padding = {
+		top = 0,
+		left = 0,
+		right = 0,
+		bottom = 0,
+	}
 end
 
 return module
--- link/common/dot-config/.config/wezterm/wezterm.lua
+++ link/common/dot-config/.config/wezterm/wezterm.lua
@@ -1,28 +1,28 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local mux = wezterm.mux
 
 function file_exists(file_path)
-    local f=io.open(file_path, "r")
-    if f~=nil then
-        io.close(f)
-        return true
-    else
-        return false
-    end
+	local f = io.open(file_path, "r")
+	if f ~= nil then
+		io.close(f)
+		return true
+	else
+		return false
+	end
 end
 
 wezterm.on("gui-attached", function(domain)
-    local is_work_machine = file_exists(os.getenv("HOME") .. "/.gitconfig-work")
-    local workspace = mux.get_active_workspace()
-    for _, window in ipairs(mux.all_windows()) do
-        if window:get_workspace() == workspace then
-            if is_work_machine then
-                window:active_pane():send_text("tmux new-session -As Work -c ~/Workspace/Code\n")
-            else
-                window:active_pane():send_text("tmux new-session -As Personal -c ~/Personal/Code\n")
-            end
-        end
-    end
+	local is_work_machine = file_exists(os.getenv("HOME") .. "/.gitconfig-work")
+	local workspace = mux.get_active_workspace()
+	for _, window in ipairs(mux.all_windows()) do
+		if window:get_workspace() == workspace then
+			if is_work_machine then
+				window:active_pane():send_text("tmux new-session -As Work -c ~/Workspace/Code\n")
+			else
+				window:active_pane():send_text("tmux new-session -As Personal -c ~/Personal/Code\n")
+			end
+		end
+	end
 end)
 
 local config = wezterm.config_builder()

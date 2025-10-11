52522b2ab41a16a465f319ad9fd86a2b1cfa9663
David Nguyen <hpcsc@users.noreply.github.com>
2025-10-11 14:45:50 +1100

    Install Fira Mono for Powerline font

--- link/common/dot-config/.config/wezterm/custom/ui.lua
+++ link/common/dot-config/.config/wezterm/custom/ui.lua
@@ -9,8 +9,7 @@ function module.apply_to_config(config)
     config.scrollback_lines = 10000
     config.hide_tab_bar_if_only_one_tab = true
     config.font = wezterm.font {
-        family = 'FiraCode Nerd Font',
-        harfbuzz_features = { 'calt=0', 'clig=0', 'liga=1' }
+        family = 'Fira Mono for Powerline',
     }
 
     config.font_size = 16

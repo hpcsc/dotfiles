c65b6f053622c4b769b567aa37bafc5b0e95f26c
David Nguyen <hpcsc@users.noreply.github.com>
2025-10-18 21:41:16 +1100

    Provide nvim-treesitter configs with lazy opts

--- link/common/dot-config/.config/nvim/lua/plugins/treesitter.lua
+++ link/common/dot-config/.config/nvim/lua/plugins/treesitter.lua
@@ -3,29 +3,30 @@ return {
 	ft = { "bash", "javascript", "typescript" },
 	build = ":TSUpdate",
 	lazy = false,
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			-- Add languages to be installed here that you want installed for treesitter
-			ensure_installed = { "bash", "javascript", "typescript" },
+	opts = {
+		-- Add languages to be installed here that you want installed for treesitter
+		ensure_installed = { "bash", "javascript", "typescript" },
 
-			-- Install parsers synchronously (only applied to `ensure_installed`)
-			sync_install = false,
+		-- Install parsers synchronously (only applied to `ensure_installed`)
+		sync_install = false,
 
-			-- Automatically install missing parsers when entering buffer
-			-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-			auto_install = false,
+		-- Automatically install missing parsers when entering buffer
+		-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+		auto_install = false,
 
-			highlight = {
-				enable = true,
-				disable = function(_, bufnr)
-					return vim.api.nvim_buf_line_count(bufnr) > 10000
-				end,
-				additional_vim_regex_highlighting = false,
-			},
+		highlight = {
+			enable = true,
+			disable = function(_, bufnr)
+				return vim.api.nvim_buf_line_count(bufnr) > 10000
+			end,
+			additional_vim_regex_highlighting = false,
+		},
 
-			indent = {
-				enable = true,
-			},
-		})
+		indent = {
+			enable = true,
+		},
+	},
+	config = function(_, opts)
+		require("nvim-treesitter.configs").setup(opts)
 	end,
 }
